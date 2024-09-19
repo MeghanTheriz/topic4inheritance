6ad9ddb6f7f8b4fd2ea884512f742a652ab84e96
MeghanTheriz <twinklerautraut711@gmail.com>
2024-09-19 15:20:09 +0800

    changing student to dog and person to animal

--- topic-4.js
+++ topic-4.js
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <style>
-        body {
-            font-family: 'Roboto', sans-serif;
-        }
-    </style>
-    <title>Topic 4 - Inheritance</title>
-</head>
-<body>
-    <h1>Topic 4 - Inheritance</h1>
-    <h1 id="personOne"></h1>
-    <script>
 class animal{
 
     //Private Fields
@@ -80,11 +63,7 @@ class Dog extends animal {
 }
 
 // Creating an instance of the Dog class
-const AnimalOne = new Dog('Bob', 'Aw aw' , 'Bite', 'Brown');
+const AnimalOne = new Dog('Cliff', 'Aw aw' , 'Bite', 'Brown');
 
 // Calling methods
 AnimalOne.displayInfo();
-AnimalOne.displayInfoHTML();
-    </script>
-</body>
-</html>
